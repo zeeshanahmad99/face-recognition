990e332d76aff4b12f479edcd82f6e2b8d418f40
Zeeshan Ahmad <zeems1234@gmail.com>
2019-11-11 04:39:59 +0500

    Updates

---
+++ precache-manifest.7d9e86d418a9fe7726a06351065463ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fef9041ba623d5c485b367512a0b646f",
+    "url": "/face-recognition/index.html"
+  },
+  {
+    "revision": "1d8943fb99139085530a",
+    "url": "/face-recognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "888330c8f443a8a21ba5",
+    "url": "/face-recognition/static/css/main.ab661361.chunk.css"
+  },
+  {
+    "revision": "1d8943fb99139085530a",
+    "url": "/face-recognition/static/js/2.95634df5.chunk.js"
+  },
+  {
+    "revision": "888330c8f443a8a21ba5",
+    "url": "/face-recognition/static/js/main.e48e2a9e.chunk.js"
+  },
+  {
+    "revision": "0f341b828c4d0124458e",
+    "url": "/face-recognition/static/js/runtime-main.1246b035.js"
+  }
+]);
